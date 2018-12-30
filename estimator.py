3b231371522f0f7711fe36a3b204375d9566e4be
Kimaru Thagana <thagana44@gmail.com>
2018-12-30 18:09:40 -0300

    examples in use

--- estimator.py
+++ estimator.py
@@ -9,3 +9,8 @@ n-sample size
 def EstimatedPopulation(observed_set):
     N=max(observed_set)+( max(observed_set)/len(observed_set) )-1
     return N
+# Problem 1- German Tanks
+print (EstimatedPopulation([1,31,43,79,115]))
+
+# Problem 2- Citti Hoppa Buses in Nairobi CBD
+print (EstimatedPopulation([663,816,458,772,584,229,717,882,422]))

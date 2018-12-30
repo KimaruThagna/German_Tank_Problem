c9138b22f3331d8ac1edca88cbfa4d86caf29c8f
Kimaru Thagana <thagana44@gmail.com>
2018-12-30 18:53:50 -0300

    extra secondary school example

--- estimator.py
+++ estimator.py
@@ -14,3 +14,6 @@ print (EstimatedPopulation([1,31,43,79,115]))
 
 # Problem 2- Citti Hoppa Buses in Nairobi CBD
 print (EstimatedPopulation([663,816,458,772,584,229,717,882,422]))
+
+#problem 3- How many students have passed through a secondary school?
+print (EstimatedPopulation([1936,1852,1741,2050,2154,2758,1909,1734,1818,2064,3040,2020,1560,5060,4788]))
